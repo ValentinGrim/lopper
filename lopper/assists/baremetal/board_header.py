931c653e4377521ff49fe1863a0d55b3a72a687f
Valentin MONNOT <valentin.monnot@st.com>
2022-05-30 09:41:41 +0200

    Write docs in board_header.py

--- lopper/assists/baremetal/board_header.py
+++ lopper/assists/baremetal/board_header.py
@@ -9,6 +9,26 @@ import sys
 
 class BoardHeader:
     def __init__(self, path):
+        """
+        This class intend to be used to as a writter.
+
+            Parameters:
+                path (str): The path where output files should be generated.
+
+            Internal vars:
+                _typdef (list): This list will contains all typedef definition.
+                                Used to be written in board_header.h.
+                _extern (list): This list will contains all extern definition.
+                                Used to be written in board_header.h.
+                _struct (ditc): This dict will contains all struct definition.
+                                Used to be written in board_header.c.
+                _const  (dict): This dict will contains all struct declaration.
+                                Used to be written in board_header.c.
+                _generated (list): This list will contains all generated tab.
+                                Used to be written in board_header.c to fill
+                                struct declaration internals vars.
+        All these internals should be filled or retrieve using getters/setters.
+        """
         try:
             self._header = open(path + "/include/board_header.h", "w")
         except OSError as err:
@@ -48,38 +68,68 @@ class BoardHeader:
 #include "board_header.h"
 """)
 
-    ##
-    #    @fn            __getitem__(self, name)
-    #    @brief        Magic method that return an item from #_struct if exist
     def __getitem__(self, name):
+        """
+        Magic method that return an item from #_struct if exist
+        """
         if name in self._struct.keys():
             return self._struct[name]
         return None
 
     def add2struct(self, struct):
+        """
+        Setter for internal _struct
+        """
         if not list(struct.keys())[0] in self._struct.keys():
             self._struct.update(struct)
 
     def add2typedef(self, typedef):
+        """
+        Setter for internal _typedef
+        """
         if not typedef in self._typedef:
             self._typedef.append(typedef)
 
     def add2extern(self, extern):
+        """
+        Setter for internal _extern
+        """
         if not extern in self._extern:
             self._extern.append(extern)
 
     def add2generated(self, generated):
+        """
+        Setter for interanl _generated
+        """
         if not generated in self._generated:
             self._generated.append(generated)
 
     def add2const(self, const, name):
+        """
+        Setter for internal _const
+        """
         if not name in self._struct.keys():
             self._const.update({name : const})
 
     def update_type(self, name, key, type_t):
+        """
+        By default, internal _struct will contains type and name for any elements.
+        When an element is pointing on a phandle, default type use to be "NULL".
+        If we generate a struct for this phandle, type should be updated to match
+        the type of the freshly generated struct.
+
+            Parameters:
+                name    (str): The name of the struct
+                key     (str): The element of the struct
+                type_t  (str): The new type for the element
+        """
         self._struct[name][key] = type_t
 
     def close(self):
+        """
+        This function has to be called at the end of the process.
+        It will write all internals vars into the .c / .h files and close them.
+        """
         # Header
         self._header.write('\n')
         for typedef in sorted(self._typedef):
